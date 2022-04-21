322a0b9add9810822da2b40d3af6f12e0288e481
Jules Duteyrat <jules.duteyrat@outlook.fr>
2022-04-21 12:06:46 +0100

    importtemplate me casse les couilles

--- ImportTemplate.js
+++ ImportTemplate.js
@@ -3,5 +3,7 @@ const dir = './ressources/templates';
 rdm = new Random();
 fs.readdir(dir, (err, files) => {
 select = rdm.nextInt(0,(files.length-1));
-fs.readFile(dir+'/'+files[select], 'utf8', (err, data) => { return '<div>'+data+'</div>'; });
+fs.readFile(dir+'/'+files[select], 'utf8', (err, data) => { 
+    print(data);
+    document.getElementById('msg').appendChild(data) });
 });
