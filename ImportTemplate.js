d9b9f543db1f0a04755f6a5ab5f60824d3dcc9da
Jules Duteyrat <jules.duteyrat@outlook.fr>
2022-04-21 16:53:26 +0100

    chargement des fichiers templates

--- ImportTemplate.js
+++ ImportTemplate.js
@@ -1,14 +1,2 @@
-const fs = require('fs');
-const dir = './ressources/templates';
-rdm = new Random();
-fs.readdir(dir, (err, files) => {
-select = rdm.nextInt(0,(files.length));
-fs.readFile(dir+'/'+files[select], 'utf8', (err, data) => { 
-        print(data);
-        const para = document.createElement("p");
-        const node = document.createTextNode(data);
-        para.appendChild(node);
-
-        const element = document.getElementById("msg");
-        element.appendChild(para); });
-});
+const dir = '../ressources/templates';
+document.getElementById("insert").innerHTML = "<div w3-include-html=\"../ressources/templates/example.html\"></div>"
