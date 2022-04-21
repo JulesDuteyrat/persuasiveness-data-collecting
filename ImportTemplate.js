98e4e540c3d13a6c8d4c58153036cc13076d70d2
Jules Duteyrat <jules.duteyrat@outlook.fr>
2022-04-21 14:59:17 +0100

    Merge branch 'main' of https://github.com/JulesDuteyrat/persuasiveness-data-collecting

--- ImportTemplate.js
+++ ImportTemplate.js
@@ -2,8 +2,13 @@ const fs = require('fs');
 const dir = './ressources/templates';
 rdm = new Random();
 fs.readdir(dir, (err, files) => {
-select = rdm.nextInt(0,(files.length-1));
+select = rdm.nextInt(0,(files.length));
 fs.readFile(dir+'/'+files[select], 'utf8', (err, data) => { 
-    print(data);
-    document.getElementById('msg').appendChild(data) });
+        print(data);
+        const para = document.createElement("p");
+        const node = document.createTextNode(data);
+        para.appendChild(node);
+
+        const element = document.getElementById("msg");
+        element.appendChild(para); });
 });
