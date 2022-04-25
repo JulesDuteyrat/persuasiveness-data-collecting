72b6a3f0e29b260de3c1e236d8d935e403e4dc2c
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2022-04-25 14:35:07 +0100

    Website now running with VueJS to have a dynamic front end with

---
+++ website/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import ButtonsVue from './components/Buttons.vue';
+</script>
+
+<template>
+  <div class="text-center mt-60 mx-auto">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + Vite" />
+    <ButtonsVue />
+  </div>
+</template>
+
+
+
+<!-- <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style> -->
---
+++ website/src/components/Buttons.vue
@@ -0,0 +1,36 @@
+<script>
+    export default {
+        data() {
+            return {
+                timerCount: 10
+            }
+        },
+        watch: {
+            timerCount: {
+                handler(value) {
+
+                    if (value > 0) {
+                        setTimeout(() => {
+                            this.timerCount--;
+                        }, 1000);
+                    }
+                },
+                immediate: true // This ensures the watcher is triggered upon creation
+            }
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+
+<template>
+    <div class=" inset-x-0 bottom-0 h-16 grid grid-cols-2 fixed p-1">
+        <button class="bg-blue-100 mr-1">quit</button>
+        <button :disabled='true' v-if="timerCount > 0" class="bg-gray-400">skip in {{ timerCount }}</button>
+        <button v-else @click="timerCount=10" class="bg-blue-100">skip</button>
+    </div>
+</template>
+
+
