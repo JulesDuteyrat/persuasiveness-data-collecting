280a799c9bf829a081cdb4110c0f46056c928e42
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2022-05-12 16:10:13 +0100

    Added TensorFlowJS to the website prototype
    :+1:

--- website/src/main.js
+++ website/src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import mitt from 'mitt'
+import * as tf from '@tensorflow/tfjs'
 import './index.css'
 
 const emitter = mitt();
+// const model = tf.loadLayersModel(''); // load model from url
 const app = createApp(App);
 app.config.globalProperties.emitter = emitter;
 app.mount('#app');
