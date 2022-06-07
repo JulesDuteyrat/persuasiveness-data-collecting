e88016ce03368a1de021f26fcedf79898769f22e
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2022-06-07 13:47:27 +0100

    Results now have the perfect format for use in TFJS

--- website/src/components/Results.vue
+++ website/src/components/Results.vue
@@ -16,10 +16,23 @@ export default {
             counts[num] = counts[num] ? counts[num] + 1 : 1;
             }
             console.log(counts);
-            // remove the element "error" from the object
-            delete counts.error;
-            
-            return counts;
+            // remove the elements from the object
+            const todelete = ['mouseup', 'focus', 'load', 'beforeunload', 'unload', 'touchend', 'error'];
+            for (let i = 0; i < todelete.length; i++) {
+                delete counts[todelete[i]];
+            }
+
+            // convert counts to imput array list
+            const allInputs = ['blur', 'click', 'contextmenu', 'copy', 'keydown', 'keyup', 'mousedown', 'mousemove', 'mouseover', 'resize', 'scroll', 'select', 'touchmove', 'touchstart'];
+            let output = [];
+            for (let i = 0; i < allInputs.length; i++) {
+                if (counts[allInputs[i]]) {
+                    output.push(counts[allInputs[i]]);
+                } else {
+                    output.push(0);
+                }
+            }
+            return output;
         }
     }
 }
